ecabd2a1a4e16dc8a45af165279e9a04baa5c847
Yama-Souichirou <souichirou19960210@gmail.com>
2022-01-11 23:36:46 +0900

    お試しように users の index を作ってみる

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+
+user_names = ["Mike", "Bob", "Risa", "Emma", "Tom", "Ted"]
+user_names.each do |name|
+  User.find_or_initialize_by(name: name, email: "#{name}@example.com").save!
+end
